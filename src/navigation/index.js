7474bae046013189a65b195a7e7a3e1573f0f3fe
Kağan Değirmen <kagandegirmen@gmail.com>
2023-10-07 18:41:12 +0300

    project init

---
+++ src/navigation/index.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation() {
+  <NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>;
+}
+
+export default AppNavigation;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, Button } from "react-native";
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+export default HomeScreen;
